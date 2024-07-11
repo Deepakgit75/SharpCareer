998d265e3af4615d42a3c62bc56a57a315cebdf0
Deepak Malviya <107317546+Deepakgit75@users.noreply.github.com>
2024-07-11 12:45:21 +0530

    Update ServiceExploration.js

--- src/components/ServiceExploration.js
+++ src/components/ServiceExploration.js
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import service from '../assets/service-exploration.jpeg';
-// import { FaArrowRightLong } from "react-icons/fa6";
-
-// const ServiceExploration = () => {
-//   return (
-//     <section className="py-10 px-5">
-//       <div className="container mx-auto flex items-center justify-center">
-//         <div className="flex-1 p-6 w-full px-10 ">
-//           <img className='h-72 w-[80%]' src={service} alt="Explore Services" />
-//         </div>
-//         <div className="flex-1">
-//           <h2 className="text-4xl  text-wrap w-[40%] font-bold mb-4">Explore our <span className='text-green-600'> Services</span></h2>
-//           <a href="/services" className="bg-blue-700 text-white flex flex-row w-28 gap-2 items-center text-lg px-4 py-2 rounded"> Services <FaArrowRightLong className=' text-xl font-semibold'/> </a>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default ServiceExploration;
-
-
-
 import React from "react";
 import service from "../assets/service-exploration.jpeg";
 import { FaArrowRightLong } from "react-icons/fa6";
